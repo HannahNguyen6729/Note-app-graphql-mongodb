c821b1d859f7ba3aec3b228ca77c29e164e3abc5
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-07-26 20:11:14 +0300

    use useLoaderData hook to get data from loader func

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,29 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { Box, Grid, Typography } from "@mui/material";
+import React from 'react';
+import { Outlet, useLoaderData } from 'react-router-dom';
+import { Box, Grid, Typography } from '@mui/material';
 
-import FolderList from "../components/FolderList";
-import UserMenu from "../components/UserMenu";
+import FolderList from '../components/FolderList';
+import UserMenu from '../components/UserMenu';
 const Home = () => {
+  const res = useLoaderData();
+  const folders = res.data.folders;
   return (
     <>
-      <Typography variant="h4" sx={{ mb: "20px" }}>
+      <Typography variant="h4" sx={{ mb: '20px' }}>
         Note App
       </Typography>
-      <Box sx={{ display: "flex", justifyContent: "right", mb: "10px" }}>
+      <Box sx={{ display: 'flex', justifyContent: 'right', mb: '10px' }}>
         <UserMenu />
       </Box>
 
       <Grid
         container
-        sx={{ height: "50vh", boxShadow: "0 0 15px 0 rgb(193 193 193 / 60%)" }}
+        sx={{ height: '50vh', boxShadow: '0 0 15px 0 rgb(193 193 193 / 60%)' }}
       >
-        <Grid item xs={3} sx={{ height: "100%" }}>
-          <FolderList
-            folders={[
-              { id: 1, name: "desktop" },
-              { id: 2, name: "work" },
-              { id: 3, name: "school" },
-            ]}
-          />
+        <Grid item xs={3} sx={{ height: '100%' }}>
+          <FolderList folders={folders} />
         </Grid>
-        <Grid item xs={9} sx={{ height: "100%" }}>
+        <Grid item xs={9} sx={{ height: '100%' }}>
           <Outlet />
         </Grid>
       </Grid>
