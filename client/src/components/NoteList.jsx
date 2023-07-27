b331a9f998cb71e87cad10f113c7497d9d60a4b3
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-07-27 17:46:25 +0300

    render folder data, notes in NoteList component

--- client/src/components/NoteList.jsx
+++ client/src/components/NoteList.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link, Outlet, useParams } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, Outlet, useLoaderData, useParams } from 'react-router-dom';
 import {
   Box,
   Card,
@@ -9,26 +9,15 @@ import {
   List,
   Tooltip,
   Typography,
-} from "@mui/material";
-import { NoteAddOutlined } from "@mui/icons-material";
+} from '@mui/material';
+import { NoteAddOutlined } from '@mui/icons-material';
 
 const NoteList = () => {
-  const { folderId, noteId } = useParams();
+  const { noteId } = useParams();
   const [activeNoteId, setActiveNoteId] = useState(noteId);
 
-  const folders = [
-    {
-      id: 1,
-      name: "desktop",
-      notes: [
-        { id: 1, content: "note1" },
-        { id: 2, content: "note2" },
-      ],
-    },
-    { id: 2, name: "work" },
-    { id: 3, name: "school" },
-  ];
-  const foundFolder = folders.find((folder) => folder.id == folderId);
+  const response = useLoaderData();
+  const detailFolder = response.data.folder;
 
   return (
     <Grid container height="100%">
@@ -36,25 +25,25 @@ const NoteList = () => {
         item
         xs={4}
         sx={{
-          width: "100%",
+          width: '100%',
           maxWidth: 360,
-          bgcolor: "#F0EBE3",
-          height: "100%",
-          overflowY: "auto",
-          padding: "10px",
-          textAlign: "left",
+          bgcolor: '#F0EBE3',
+          height: '100%',
+          overflowY: 'auto',
+          padding: '10px',
+          textAlign: 'left',
         }}
       >
         <List
           subheader={
             <Box
               sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
               }}
             >
-              <Typography sx={{ fontWeight: "bold" }}>Notes</Typography>
+              <Typography sx={{ fontWeight: 'bold' }}>Notes</Typography>
               <Tooltip
                 title="Add Note"
                 // onClick={handleAddNewNote}
@@ -66,31 +55,31 @@ const NoteList = () => {
             </Box>
           }
         >
-          {foundFolder.notes?.map(({ id, content }) => {
+          {detailFolder.notes?.map(({ id, content }) => {
             return (
               <Link
                 key={id}
                 to={`note/${id}`}
-                style={{ textDecoration: "none" }}
+                style={{ textDecoration: 'none' }}
                 onClick={() => setActiveNoteId(id)}
               >
                 <Card
                   sx={{
-                    mb: "5px",
+                    mb: '5px',
                     backgroundColor:
-                      id === activeNoteId ? "rgb(255 211 140)" : null,
+                      id === activeNoteId ? 'rgb(255 211 140)' : null,
                   }}
                 >
                   <CardContent
-                    sx={{ "&:last-child": { pb: "10px" }, padding: "10px" }}
+                    sx={{ '&:last-child': { pb: '10px' }, padding: '10px' }}
                   >
                     <div
-                      style={{ fontSize: 14, fontWeight: "bold" }}
+                      style={{ fontSize: 14, fontWeight: 'bold' }}
                       dangerouslySetInnerHTML={{
-                        __html: `${content.substring(0, 30) || "Empty"}`,
+                        __html: `${content.substring(0, 30) || 'Empty'}`,
                       }}
                     />
-                    <Typography sx={{ fontSize: "10px" }}></Typography>
+                    <Typography sx={{ fontSize: '10px' }}></Typography>
                   </CardContent>
                 </Card>
               </Link>
