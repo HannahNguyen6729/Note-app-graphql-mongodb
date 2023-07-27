fe2faa7b5006be981a186087329aac4df0ed6e2d
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-07-27 18:10:42 +0300

    remove react strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes";
-import "./firebase/config";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
+import './firebase/config';
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { Container } from "@mui/material";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Container } from '@mui/material';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Container maxWidth="lg" sx={{ textAlign: "center", marginTop: "50px" }}>
-      <RouterProvider router={router} />
-    </Container>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Container maxWidth="lg" sx={{ textAlign: 'center', marginTop: '50px' }}>
+    <RouterProvider router={router} />
+  </Container>
 );
